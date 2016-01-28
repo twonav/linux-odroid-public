40945caf77e82060df74bf86b9c8a45bb275e2d6
Tobias Jakobi <tjakobi@math.uni-bielefeld.de>
2016-01-28 16:27:50 +0100

    WIP: drm/exynos: mixer: enable NV12MT

--- drivers/gpu/drm/exynos/exynos_mixer.c
+++ drivers/gpu/drm/exynos/exynos_mixer.c
@@ -154,7 +154,8 @@ static const struct exynos_drm_plane_config plane_configs[MIXER_WIN_NR] = {
 		.pixel_formats = vp_formats,
 		.num_pixel_formats = ARRAY_SIZE(vp_formats),
 		.capabilities = EXYNOS_DRM_PLANE_CAP_SCALE |
-				EXYNOS_DRM_PLANE_CAP_ZPOS,
+				EXYNOS_DRM_PLANE_CAP_ZPOS |
+				EXYNOS_DRM_PLANE_CAP_TILE,
 	},
 };
 
@@ -551,6 +552,9 @@ static void vp_video_buffer(struct mixer_context *ctx,
 				fb->pixel_format);
 		return;
 	}
+
+	if (fb->modifier[0] == DRM_FORMAT_MOD_SAMSUNG_64_32_TILE)
+		tiled_mode = true;
 
 	luma_addr[0] = exynos_drm_fb_dma_addr(fb, 0);
 	chroma_addr[0] = exynos_drm_fb_dma_addr(fb, 1);
