8c24de4099145f2c45e48a5cfb675c9b1fa62e85
Tobias Jakobi <tjakobi@math.uni-bielefeld.de>
2016-03-27 23:31:58 +0200

    drm/exynos: g2d: remove EXYNOS_G2D_GET_VER ioctl
    This ioctl is superseded by EXYNOS_G2D_GET_VER2.

--- drivers/gpu/drm/exynos/exynos_drm_g2d.c
+++ drivers/gpu/drm/exynos/exynos_drm_g2d.c
@@ -1058,32 +1058,6 @@ static void g2d_cmdlist_prolog(struct g2d_cmdlist *cmdlist, bool event)
 }
 
 /* ioctl functions */
-int exynos_g2d_get_ver_ioctl(struct drm_device *drm_dev, void *data,
-			     struct drm_file *file)
-{
-	struct drm_exynos_file_private *file_priv = file->driver_priv;
-	struct exynos_drm_g2d_private *g2d_priv = file_priv->g2d_priv;
-	struct device *dev;
-	struct g2d_data *g2d;
-	struct drm_exynos_g2d_get_ver *ver = data;
-
-	if (!g2d_priv)
-		return -ENODEV;
-
-	dev = g2d_priv->dev;
-	if (!dev)
-		return -ENODEV;
-
-	g2d = dev_get_drvdata(dev);
-	if (!g2d)
-		return -EFAULT;
-
-	ver->major = G2D_HW_MAJOR_VER;
-	ver->minor = G2D_HW_MINOR_VER;
-
-	return 0;
-}
-
 int exynos_g2d_get_ver2_ioctl(struct drm_device *drm_dev, void *data,
 			     struct drm_file *file)
 {
