1c7d3e709ba67e317b849634d6e7d79cd2b93f2f
Tobias Jakobi <tjakobi@math.uni-bielefeld.de>
2016-03-27 19:28:06 +0200

    drm/exynos: g2d: add EXYNOS_G2D_GET_VER2 ioctl
    This extends the EXYNOS_G2D_GET_VER ioctl with a 'caps' field
    enabling userspace to query capabilities of the G2D driver.

--- drivers/gpu/drm/exynos/exynos_drm_g2d.c
+++ drivers/gpu/drm/exynos/exynos_drm_g2d.c
@@ -1084,6 +1084,33 @@ int exynos_g2d_get_ver_ioctl(struct drm_device *drm_dev, void *data,
 	return 0;
 }
 
+int exynos_g2d_get_ver2_ioctl(struct drm_device *drm_dev, void *data,
+			     struct drm_file *file)
+{
+	struct drm_exynos_file_private *file_priv = file->driver_priv;
+	struct exynos_drm_g2d_private *g2d_priv = file_priv->g2d_priv;
+	struct device *dev;
+	struct g2d_data *g2d;
+	struct drm_exynos_g2d_get_ver2 *ver = data;
+
+	if (!g2d_priv)
+		return -ENODEV;
+
+	dev = g2d_priv->dev;
+	if (!dev)
+		return -ENODEV;
+
+	g2d = dev_get_drvdata(dev);
+	if (!g2d)
+		return -EFAULT;
+
+	ver->major = G2D_HW_MAJOR_VER;
+	ver->minor = G2D_HW_MINOR_VER;
+	ver->caps = 0;
+
+	return 0;
+}
+
 int exynos_g2d_set_cmdlist_ioctl(struct drm_device *drm_dev, void *data,
 				 struct drm_file *file)
 {
